d199ceae6cb80ba909c1e1284bc11835111aa3fe
Juan Marcos <juan.demiguelfunes@gmail.com>
2025-08-26 20:32:09 +0000

    agregue unas configuraciones pedorras

--- nvim/.config/nvim/lua/juanm/lazy/lsp.lua
+++ nvim/.config/nvim/lua/juanm/lazy/lsp.lua
@@ -219,10 +219,10 @@ local lsp_plugins = {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
+				clangd = {},
+				gopls = {},
+				pyright = {},
+				rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
--- nvim/.config/nvim/lua/juanm/remap.lua
+++ nvim/.config/nvim/lua/juanm/remap.lua
@@ -93,3 +93,6 @@ vim.keymap.set("n", "<leader><leader>", function()
 end)
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
